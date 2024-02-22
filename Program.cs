939aa571c6b902bed0b372df54d6e8cb37686775
Kieran Emery <kizwiz@hotmail.co.uk>
2024-02-22 14:57:05 +0000

    Adds Taj Mahal and Great Mosque of Mecca.

--- Program.cs
+++ Program.cs
@@ -12,6 +12,16 @@
             Console.WriteLine(totalArea);
             Console.WriteLine(totalCost);
             Console.WriteLine($"My plan costs: {Math.Round(totalCost, 2)} pesos.");
+
+            double totalAreaTM = Rect(90.5, 90.5) - 2 * Rect(24, 24);
+            double totalCostTM = totalAreaTM * 180;
+            Console.WriteLine(totalCostTM);
+            Console.WriteLine($"Taj Mahal plan costs: {Math.Round(totalCostTM, 2)} pesos.");
+
+            double totalAreaGMM = Rect(180, 106) + Rect(200, 264) + Triangle(264, 84);
+            double totalCostGMM = totalAreaGMM * 180;
+            Console.WriteLine(totalCostGMM);
+            Console.WriteLine($"Great Mosque of Mecca plan costs: {Math.Round(totalCostGMM, 2)} pesos.");
         }
 
         static double Rect(double length, double width)
