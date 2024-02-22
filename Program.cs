1695397f426b739f8cbf3f6ddb61669fc0df2497
Kieran Emery <kizwiz@hotmail.co.uk>
2024-02-22 15:08:20 +0000

    Refactoring code to include a switch statement and gathers user input.

--- Program.cs
+++ Program.cs
@@ -4,24 +4,29 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(Rect(10, 5));
-            Console.WriteLine(Circle(5));
-            Console.WriteLine(Triangle(10, 5));
-            double totalArea = Rect(2500, 1500) + Triangle(750, 500) + 0.5 * Circle(375);
+            Console.WriteLine("What monument would you like to work with?");
+            Console.Write("Choose Pantheon (1), Taj Mahal (2), or Great Mosque of Mecca (3): ");
+            string monument = Console.ReadLine();
+            double totalArea;
+
+            switch (monument)
+            {
+                case "1":
+                    totalArea = Rect(2500, 1500) + Triangle(750, 500) + 0.5 * Circle(375);
+                    break;
+                case "2":
+                    totalArea = Rect(90.5, 90.5) - 2 * Rect(24, 24);
+                    break;
+                case "3":
+                    totalArea = Rect(180, 106) + Rect(200, 264) + Triangle(264, 84);
+                    break;
+                default:
+                    totalArea = 0;
+                    Console.Write("No monument selected. ");
+                    break;
+            }
             double totalCost = totalArea * 180;
-            Console.WriteLine(totalArea);
-            Console.WriteLine(totalCost);
             Console.WriteLine($"My plan costs: {Math.Round(totalCost, 2)} pesos.");
-
-            double totalAreaTM = Rect(90.5, 90.5) - 2 * Rect(24, 24);
-            double totalCostTM = totalAreaTM * 180;
-            Console.WriteLine(totalCostTM);
-            Console.WriteLine($"Taj Mahal plan costs: {Math.Round(totalCostTM, 2)} pesos.");
-
-            double totalAreaGMM = Rect(180, 106) + Rect(200, 264) + Triangle(264, 84);
-            double totalCostGMM = totalAreaGMM * 180;
-            Console.WriteLine(totalCostGMM);
-            Console.WriteLine($"Great Mosque of Mecca plan costs: {Math.Round(totalCostGMM, 2)} pesos.");
         }
 
         static double Rect(double length, double width)
